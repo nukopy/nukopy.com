e536a2fd6dc5cbda8d2ae4760760cba3514d275b
nukopy <pytwbf201830@gmail.com>
2021-04-27 01:14:16 +0900

    feat: use three.js

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,43 @@
 <script lang="ts">
-	export let name: string;
+  import {
+    Canvas,
+    Scene,
+    PerspectiveCamera,
+    DirectionalLight,
+    AmbientLight,
+    BoxBufferGeometry,
+    Mesh,
+    MeshStandardMaterial,
+    WebGLRenderer,
+  } from "svelthree";
+
+  let cubeGeometry = new BoxBufferGeometry(1, 1, 1);
+  let cubeMaterial = new MeshStandardMaterial();
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Canvas let:sti w={500} h={500}>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  <Scene {sti} let:scene id="scene1" props={{ background: 0xedf2f7 }}>
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    <PerspectiveCamera {scene} id="cam1" pos={[0, 0, 3]} lookAt={[0, 0, 0]} />
+    <AmbientLight {scene} intensity={1.25} />
+    <DirectionalLight {scene} pos={[3, 3, 3]} />
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+    <Mesh
+      {scene}
+      geometry={cubeGeometry}
+      material={cubeMaterial}
+      mat={{ roughness: 0.5, metalness: 0.5, color: 0xff3e00 }}
+      pos={[0, 0, 0]}
+      rot={[0.5, 0.6, 0]}
+      scale={[1, 1, 1]} />
+
+  </Scene>
+
+  <WebGLRenderer
+    {sti}
+    sceneId="scene1"
+    camId="cam1"
+    config={{ antialias: true, alpha: false }} />
+
+</Canvas>
