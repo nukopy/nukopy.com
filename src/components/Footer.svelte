df788ec0eadba53147bdd74f27538e946e07f4da
nukopy <pytwbf201830@gmail.com>
2021-05-22 23:05:16 +0900

    feat: add Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,10 @@
+<footer>
+    &copy; <a href="https://github.com/nukopy">nukopy</a><br />
+    Powerd by Svelte + Sapper + Vercel
+</footer>
+
+<style lang="scss">
+footer {
+    background-color: rgb(177, 173, 173);
+}
+</style>
