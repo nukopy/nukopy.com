dfac025cd223d2f206406bbea9f208e982524c54
nukopy <pytwbf201830@gmail.com>
2021-05-22 23:51:06 +0900

    format

--- src/routes/blog/[slug].json.js
+++ src/routes/blog/[slug].json.js
@@ -6,12 +6,12 @@ export async function get(req, res) {
   const API_URL = `https://${serviceName}.microcms.io/api/v1/blog/${slug}`;
 
   fetch(API_URL, {
-    headers: { "X-API-KEY": apiKey },
+    headers: { 'X-API-KEY': apiKey },
   })
     .then((res) => res.json())
     .then((json) => {
       res.writeHead(200, {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       });
       res.end(JSON.stringify(json));
     });
--- src/routes/blog/index.json.js
+++ src/routes/blog/index.json.js
@@ -1,4 +1,4 @@
-import fetch from "isomorphic-unfetch";
+import fetch from 'isomorphic-unfetch';
 
 const serviceName = process.env.SERVICE_NAME;
 const API_URL = `https://${serviceName}.microcms.io/api/v1/blog/`;
@@ -6,12 +6,12 @@ const apiKey = process.env.API_KEY;
 
 export async function get(req, res) {
   fetch(API_URL, {
-    headers: { "X-API-KEY": apiKey },
+    headers: { 'X-API-KEY': apiKey },
   })
     .then((res) => res.json())
     .then((json) => {
       res.writeHead(200, {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       });
       res.end(JSON.stringify(json));
     });
