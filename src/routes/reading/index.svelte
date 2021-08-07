7ad3696a5d38e1fd0aec7e3443fb34dc54b4c98b
nukopy <pytwbf201830@gmail.com>
2021-08-07 13:41:26 +0900

    feat: add a content "reading" to show books I read

---
+++ src/routes/reading/index.svelte
@@ -0,0 +1,111 @@
+<svelte:head>
+  <title>Reading</title>
+</svelte:head>
+
+<h1>Reading</h1>
+
+<p>読んだ本、資料など</p>
+
+<!--  -->
+<h2>学問</h2>
+
+<!--  -->
+<h3>コンピュータサイエンス</h3>
+<h4>基礎</h4>
+<h5>離散数学</h5>
+<h5>アルゴリズムとデータ構造</h5>
+
+<!-- Software -->
+<h4>ソフトウェア</h4>
+<h5>システムソフトウェア</h5>
+<ul>
+  <li>OS</li>
+  <li>言語処理系</li>
+  <li>ミドルウェア</li>
+  <ul>
+    <li>DBMS（MySQL、PostgreSQL、MongoDB、etc...）</li>
+    <li>TCP/IP ソフトウェア</li>
+  </ul>
+</ul>
+<h5>アプリケーションソフトウェア</h5>
+
+<h5>プログラミング言語</h5>
+<ul>
+  <li>C</li>
+  <ul>
+    <li><a href="https://9cguide.appspot.com/">苦しんで覚える C 言語</a></li>
+    <li
+      >（読み中）<a href="https://kremlin.cc/k&r.pdf"
+        >The Programing Language C 2nd Edition</a
+      ></li
+    >
+  </ul>
+  <li>C++</li>
+  <ul>
+    <li>やさしい C++</li>
+    <li>（読み中）新・明解 C++ 入門</li>
+  </ul>
+  <li>JavaScript</li>
+  <ul>
+    <li>
+      <a href="https://jsprimer.net/">JavaScript Primer</a>
+    </li>
+  </ul>
+  <li>TypeScript</li>
+  <ul>
+    <li
+      ><a href="https://future-architect.github.io/typescript-guide/index.html"
+        >仕事で使える TypeScript</a
+      ></li
+    >
+  </ul>
+  <li>Python</li>
+  <li>Golang</li>
+  <li>SQL</li>
+</ul>
+
+<!-- Hardware -->
+<h4>ハードウェア</h4>
+<ul>
+  <li>CPU（プロセッサ、マイクロプロセッサ）</li>
+  <li>記憶装置</li>
+  <li>入力装置</li>
+</ul>
+
+<h4>ネットワーク</h4>
+<h4>応用分野</h4>
+
+<!--  -->
+<h3>生物学</h3>
+<h4>生化学</h4>
+<h4>分子生物学</h4>
+
+<!--  -->
+<h3>社会科</h3>
+<h4>全般</h4>
+<h4>地理</h4>
+<h4>政治経済</h4>
+<h4>歴史</h4>
+<h5>日本史</h5>
+<h5>世界史</h5>
+
+<!--  -->
+<h3>哲学</h3>
+
+<!--  -->
+<h2>教養</h2>
+<h3>ビジネス書</h3>
+
+<!--  -->
+<h3>一般書</h3>
+
+<!--  -->
+<h3>小説</h3>
+
+<style>
+  ul {
+    margin: auto;
+    padding-left: 2em;
+    list-style: circle;
+  }
+</style>
