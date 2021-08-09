4f9c8915120ed0ed0ab846aa11a14633779c3a44
nukopy <pytwbf201830@gmail.com>
2021-08-10 08:52:45 +0900

    feat: add medium icon

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,15 +1,16 @@
 <script>
-  import { github, linkedin } from 'svelte-awesome/icons';
+  import { github, linkedin, medium } from 'svelte-awesome/icons';
   import SnsIcon from './SnsIcon.svelte';
 </script>
 
 <footer>
   <!-- Font Awesome: https://fontawesome.com/v5.15/icons?d=gallery&p=2&q=github -->
   <!-- svelte-awesome: https://github.com/RobBrazier/svelte-awesome -->
-  <!-- todo: 色々落ち着いたら、LinkedIn のアカウントへのリンクを貼る -->
+  <!-- todo: 定職に就いて落ち着いたら、LinkedIn のアカウントへのリンクを貼る -->
   <p>
     <SnsIcon link="https://www.linkedin.com/feed/" data={linkedin} />
     <SnsIcon link="https://github.com/nukopy" data={github} />
+    <SnsIcon link="https://medium.com/@nukopy" data={medium} />
   </p>
 
   <!-- copyright -->
