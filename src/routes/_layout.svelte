e45be230888016c5f2adb9d468205e8bbb6a2eb6
nukopy <pytwbf201830@gmail.com>
2021-08-13 12:02:33 +0900

    style: fix layout for mobile browsers

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -40,4 +40,9 @@
     min-height: 100vh;
     min-height: calc(var(--vh, 1vh) * 100);
   }
+
+  main {
+    display: flex;
+    flex-direction: column;
+  }
 </style>
--- src/routes/top/index.svelte
+++ src/routes/top/index.svelte
@@ -3,44 +3,48 @@
 </svelte:head>
 
 <div id="welcome">
-  <h1>Thanks for visiting nukopy's portfolio!</h1>
+  <h1>Thanks for<br />visiting<br />nukopy's portfolio!</h1>
 </div>
 
 <style type="text/scss">
   #welcome {
-    margin: 0 1rem;
-    // 上下中央揃え
-    // display: flex;
-    // justify-content: center;
-    // align-items: center;
-    display: flex;
-    place-items: center;
-    // vertical-align: center;
+    margin: auto 1rem;
+    height: 70vh;
+
+    // 左右中央寄せ
+    text-align: center;
+    display: table;
   }
 
   h1 {
-    // 左右中央揃え
-    text-align: center;
-    margin: 0 auto;
+    // 上下中央寄せ
+    // hoge
+    display: table-cell;
+    vertical-align: middle;
+
     // font
-    font-size: 4em;
+    font-size: 6em;
     text-transform: uppercase;
     font-weight: 700;
   }
 
   @media screen and (max-width: 1000px) {
     h1 {
-      font-size: 3rem;
+      font-size: 5rem;
     }
   }
 
   @media screen and (max-width: 768px) {
     h1 {
-      font-size: 3rem;
+      font-size: 5rem;
     }
   }
 
   @media screen and (max-width: 400px) {
+    #welcome {
+      height: 60vh;
+    }
+
     h1 {
       font-size: 3rem;
     }
