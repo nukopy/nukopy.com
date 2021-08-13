9b077f6f249786ff68d23bed8c0ff6e30a255f8c
nukopy <pytwbf201830@gmail.com>
2021-08-13 09:52:37 +0900

    [wip] style: fix the position of footer

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import Nav from '../components/Nav.svelte';
   import Footer from '../components/Footer.svelte';
 
   export let segment: string;
+
+  const setFillHeight = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    // re-calculate when screen size change
+    window.addEventListener('resize', setFillHeight);
+  };
+
+  onMount(() => {
+    setFillHeight();
+  });
 </script>
 
 <div id="container">
@@ -19,4 +32,11 @@
   @import '../styles/global.scss';
   @import '../styles/variables.scss';
   @import '../styles/theme.scss';
+
+  #container {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    min-height: calc(var(--vh, 1vh) * 100);
+  }
 </style>
