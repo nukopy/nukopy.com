eca9301e4be672708441bcf824be22c4742d582a
nukopy <pytwbf201830@gmail.com>
2021-10-12 21:21:27 +0900

    feat: add a list of components of computer to "Works"

--- src/routes/works/index.svelte
+++ src/routes/works/index.svelte
@@ -5,3 +5,45 @@
 <h1>Works</h1>
 
 <p><b>Under Construction...</b></p>
+
+<!-- ref: https://nukopy.notion.site/nukopy/Wiki-for-Study-720124b9661c40b98805429142401f4a -->
+
+<ul>
+  <li>Software</li>
+  <ul>
+    <li>System Software</li>
+    <ul>
+      <li>OS</li>
+
+      <li>Language Processor</li>
+      <ul>
+        <li>Compiler</li>
+        <li>Interpreter</li>
+        <li>Virtual Machine</li>
+      </ul>
+
+      <li>Middleware</li>
+      <ul>
+        <li>DBMS(MySQL, PostgreSQL, MongoDB, etc...)</li>
+        <li>TCP/IP Software</li>
+        <li>HTTP Server(Nginx, Apache HTTP Server)</li>
+      </ul>
+    </ul>
+
+    <li>Application Software</li>
+    <ul>
+      <li>Web Application</li>
+      <li>Mobile Application</li>
+      <li>Cross Platform Application</li>
+      <li>CLI Tools</li>
+      <li>Others</li>
+    </ul>
+  </ul>
+
+  <li>Hardware</li>
+  <ul>
+    <li>CPU(processor, micro processor)</li>
+    <li>Storage Unit</li>
+    <li>Input Unit</li>
+  </ul>
+</ul>
