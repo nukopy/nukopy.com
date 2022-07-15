e5eead2f0e687b5177c3314ef472a0aeebaea38c
nukopy <pytwbf201830@gmail.com>
2022-07-16 02:23:40 +0900

    remove: delete svelter / sapper projects

--- src/routes/about/index.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-  <title>About</title>
-</svelte:head>
-
-<h1>About</h1>
-
-<p> I'm a software developer. </p>
--- src/routes/scrap/index.svelte
+++
@@ -1,9 +0,0 @@
-<svelte:head>
-  <title>Scrap</title>
-</svelte:head>
-
-<h1>Scrap</h1>
-
-<p><b>Under Construction...</b></p>
-
-<!-- todo: Scrapbox の tech-notes の API からスクラップを取ってくる -->
