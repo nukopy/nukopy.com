f0a0fa273073bf439b6cf083b2d09e158ca8193f
nukopy <pytwbf201830@gmail.com>
2022-07-16 04:10:15 +0900

    chore: add import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === "serve") {
+    return {
+      // base: "/src/",
+      resolve: {
+        alias: {
+          "@/": `${__dirname}/src/`,
+        },
+      },
+      mode: mode,
+      plugins: [react()],
+    };
+  }
+
+  throw new Error(`Unknown command: "${command}"`);
 });
