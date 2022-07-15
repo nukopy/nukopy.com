94db2b705f90039c1e8cb540d4e872a8105a8e17
nukopy <pytwbf201830@gmail.com>
2022-07-16 04:10:40 +0900

    feat: add base page

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,11 @@
+export const AboutPage = () => {
+  return (
+    <>
+      <h1>Works</h1>
+
+      <p>
+        <b>I'm a software Developer.</b>
+      </p>
+    </>
+  );
+};
---
+++ src/pages/Works/index.tsx
@@ -0,0 +1,51 @@
+export const WorksPage = () => {
+  return (
+    <>
+      <h1>Works</h1>
+
+      <p>
+        <b>Under Construction...</b>
+      </p>
+
+      <ul>
+        <li>Software</li>
+        <ul>
+          <li>System Software</li>
+          <ul>
+            <li>OS</li>
+
+            <li>Language Processor</li>
+            <ul>
+              <li>Compiler</li>
+              <li>Interpreter</li>
+              <li>Virtual Machine</li>
+            </ul>
+
+            <li>Middleware</li>
+            <ul>
+              <li>DBMS(MySQL, PostgreSQL, MongoDB, etc...)</li>
+              <li>TCP/IP Software</li>
+              <li>HTTP Server(Nginx, Apache HTTP Server)</li>
+            </ul>
+          </ul>
+
+          <li>Application Software</li>
+          <ul>
+            <li>Web Application</li>
+            <li>Mobile Application</li>
+            <li>Cross Platform Application</li>
+            <li>CLI Tools</li>
+            <li>Others</li>
+          </ul>
+        </ul>
+
+        <li>Hardware</li>
+        <ul>
+          <li>CPU(processor, micro processor)</li>
+          <li>Storage Unit</li>
+          <li>Input Unit</li>
+        </ul>
+      </ul>
+    </>
+  );
+};
