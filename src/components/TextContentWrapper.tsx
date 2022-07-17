2e2a69e23d366fdab25c15ff38429338c3dcd53e
nukopy <pytwbf201830@gmail.com>
2022-07-18 03:31:06 +0900

    feat: impl wrapper component for text contents

---
+++ src/components/TextContentWrapper.tsx
@@ -0,0 +1,9 @@
+import { styled, Box } from "@mui/material";
+
+export const TextContentWrapper = styled(Box)({
+  flexGrow: 1,
+  paddingTop: "1rem",
+  paddingBottom: "1rem",
+  paddingLeft: "1rem",
+  paddingRight: "1rem",
+});
