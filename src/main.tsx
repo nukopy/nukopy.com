2e49aebe9c807be7f53607a9ebb8f4a8d020b10d
nukopy <pytwbf201830@gmail.com>
2022-07-18 03:34:48 +0900

    feat: remove reset.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "@/style/reset.css";
+
 import "@/style/index.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
