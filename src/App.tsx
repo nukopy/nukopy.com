7a643277862a56f96f2b640f0bec10fdf143754e
nukopy <pytwbf201830@gmail.com>
2022-07-18 03:34:19 +0900

    feat: impl themeing, routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { HomePage } from "@/pages/Home";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+import { routes } from "@/routes";
+import { ThemeProvider } from "./components/ThemeProvider";
+import Layout from "./components/Layout";
 
 const App = () => {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <ThemeProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            {routes.map((route) => {
+              return (
+                <Route
+                  key={route.key}
+                  path={route.path}
+                  element={<route.component />}
+                />
+              );
+            })}
+          </Routes>
+        </Layout>
+      </Router>
+    </ThemeProvider>
   );
 };
 
